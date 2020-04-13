0cb7a8cb3bd4422350c05930c8177d95d002f9fd
Jens Lange <lange.jens@outlook.com>
2020-04-13 19:22:37 +0200

    Add possibility to fit power-law

--- R/cpCommunitySizeDistribution.R
+++ R/cpCommunitySizeDistribution.R
@@ -1,23 +1,33 @@
 #' Plotting Clique Percolation Community Size Distribution
 #' 
 #' Function for plotting the frequency distribution of community sizes from clique
-#' percolation community detection.
+#' percolation community detection and testing for power-law.
 #' 
 #' @param list.of.communities List object taken from results of cpAlgorithm
 #'    function; see also \link{cpAlgorithm}
 #' @param color.line string indicating the color of the line in the plot as described
 #'    in \link[graphics]{par}; default is \code{"#bc0031"}
+#' @param test.power.law Logical indicating whether fit of power-law should be tested; default
+#'    is FALSE; see Details
 #'    
 #' @return
 #'   The function primarily plots the community size distribution. Additionally, it returns
 #'   a list with a data frame containing all community sizes and their frequencies
-#'   (\code{size.distribution}).
+#'   (\code{size.distribution}). If \code{test.power.law = TRUE}, a test of fit of a power-law
+#'   distribution is also returned as a list object with results from fit_power_law (see
+#'   also \link[igraph]{fit_power_law}.
 #' 
 #' @details
 #'   The function takes the results of cpAlgorithm (see also \link{cpAlgorithm}),
 #'   that is, either the \code{list.of.communities.numbers} or the
 #'   \code{list.of.communities.labels} and plots the community size distribution. If there
 #'   are no communities, no plot can be generated. An error is printed indicating this.
+#'   
+#'   If \code{test.power.law = TRUE}, test of a fit of a power-law is performed with the
+#'   function fit_power_law (see also \link[igraph]{fit_power_law}). Fit is tested for
+#'   the entire distribution from the smallest community size k onwards (i.e., typically k
+#'   as specified in cpAlgorithm. Moreover, test uses the \code{plfit} implementation of
+#'   fit_power_law. For other arguments, default values are used.
 #' 
 #' @examples
 #' # create qgraph object; 150 nodes; 1/7 of all edges are different from zero
@@ -35,12 +45,18 @@
 #' # plot community size distribution with blue line
 #' cp.size.dist <- cpCommunitySizeDistribution(cp.results$list.of.communities.numbers,
 #'                                             color.line = "#0000ff")
+#' # test for power-law distribution
+#' cp.size.dist <- cpCommunitySizeDistribution(cp.results$list.of.communities.numbers,
+#'                                             color.line = "#0000ff",
+#'                                             test.power.law = TRUE)
+#' cp.size.dist$fit.power.law
 #' 
 #' @author Jens Lange, \email{lange.jens@@outlook.com} 
 #' 
 #' @export cpCommunitySizeDistribution
 
-cpCommunitySizeDistribution <- function(list.of.communities, color.line = "#bc0031"){
+cpCommunitySizeDistribution <- function(list.of.communities, color.line = "#bc0031",
+                                        test.power.law = FALSE){
   
   #if there are no communities...
   #print error
@@ -67,7 +83,17 @@ cpCommunitySizeDistribution <- function(list.of.communities, color.line = "#bc00
                      pch = 16, cex = 1.5)
     graphics::lines(x = size_freq$size, y = size_freq$frequency, col = color.line, lwd = 3)
     
-    return(list(size.distribution = size_freq))
+    if (test.power.law == FALSE) {
+      return(list(size.distribution = size_freq))
+    }
+    
+    if (test.power.law == TRUE) {
+      size_vector <- rep(size_freq$size, size_freq$frequency)
+      fit_pl <- igraph::fit_power_law(size_vector, xmin = min(size_vector),
+                                      implementation = "plfit")
+      return(list(size.distribution = size_freq,
+                  fit.power.law = fit_pl))
+    }
   }
   
 }
